a0747b3e0d0c7292dea2b56d25b8eeaebd1a80e6
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-09-11 19:17:39 -0400

    added routes to get all notes, and to add new notes

--- server.js
+++ server.js
@@ -13,6 +13,47 @@ app.use(express.json());
 // Serve static files from the 'public' directory
 app.use(express.static("public"));
 
+// Define the API routes becaue DRY
+const dbPath = path.join(__dirname, './db/db.json');
+
+// Route to get all notes
+app.get('/api/notes', (req, res) => {
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+      const notes = JSON.parse(data);
+      res.json(notes);
+    });
+  });
+  
+  // Route to add a new note
+  app.post('/api/notes', (req, res) => {
+    fs.readFile(dbPath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+  
+      const notes = JSON.parse(data);
+      const newNote = req.body;
+  
+      // Generate a unique ID for the new note.
+      newNote.id = uuidv4();
+  
+      notes.push(newNote);
+  
+      fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+        res.json(newNote);
+      });
+    });
+  });
+
 // Route to serve the 'notes.html' page
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "notes.html"));
