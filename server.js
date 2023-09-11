6a39386e322fb71200c3c8f5ae63dbc79a3145f0
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-09-11 19:15:44 -0400

    added routes to serve notes.html and index.html

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const { v4: uuidv4 } = require('uuid');
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,5 +9,21 @@ const PORT = process.env.PORT || 3001;
 // Middleware for parsing JSON data
 app.use(express.json());
 
+//!!Are return and serve used interchangably?
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+// Route to serve the 'notes.html' page
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+// Route to serve the 'index.html' page for all other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
