e4f20a953cb339913c5345f3090d33ee3c07d1e6
Ben Cooper <39800139+BenACooper@users.noreply.github.com>
2023-09-11 22:42:14 -0400

    successfully integrated fsUtils, turns out I started accidentalyl working on the original fsUtils from the miniproject instead of the copy in this project *facepalm*

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+//TODO Refactor code to implement modular design
+//TODO Refactor code to implemenet fsUtils?
+
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const { readFromFile, writeToFile, readAndAppend, readAndDelete } = require("./helpers/fsUtils") //Plagiarised these utiity functions from the MiniProject. xP 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -18,74 +22,51 @@ const dbPath = path.join(__dirname, "./db/db.json");
 
 // Route to get all notes refactored with readFromFile
 app.get("/api/notes", (req, res) => {
-  fs.readFile(dbPath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-    const notes = JSON.parse(data);
-    res.json(notes);
+    readFromFile(dbPath)
+      .then(data => {
+        const notes = JSON.parse(data);
+        res.json(notes);
+      })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ error: "Internal Server Error" });
+      });
   });
-});
 
-// Route to add a new note based on line 36 from index.js
+// Route to add a new note refactored with redAndAppend 
 app.post("/api/notes", (req, res) => {
-  fs.readFile(dbPath, "utf8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-
-    const notes = JSON.parse(data);
     const newNote = req.body;
-
+  
     // Generate a unique ID for the new note.
     newNote.id = uuidv4();
-
-    notes.push(newNote);
-
-    fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
-      if (err) {
+  
+    readAndAppend(newNote, dbPath)
+      .then(() => {
+        res.json(newNote);
+      })
+      .catch((err) => {
         console.error(err);
-        return res.status(500).json({ error: "Internal Server Error" });
-      }
-      res.json(newNote);
-    });
+        res.status(500).json({ error: "Internal Server Error" });
+      });
   });
-});
-
-// Route to handle DELETE requests
-// Route to delete a note by ID
+  
+// Route to handle DELETE requests by ID
 app.delete('/api/notes/:id', (req, res) => {
-  fs.readFile(dbPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-
-    const notes = JSON.parse(data);
     const noteId = req.params.id;
-
-    // Find the index of the note with the given ID
-    const noteIndex = notes.findIndex((note) => note.id === noteId);
-
-    if (noteIndex === -1) {
-      return res.status(404).json({ error: 'Note not found' });
-    }
-
-    // Remove the note with the given ID from the array
-    notes.splice(noteIndex, 1);
-
-    // Write the updated notes array back to the db.json file
-    fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
-      if (err) {
+  
+    readAndDelete(dbPath, noteId)
+      .then((deletedNoteId) => {
+        if (deletedNoteId) {
+          res.json({ message: 'Note deleted successfully' });
+        } else {
+          res.status(404).json({ error: 'Note not found' });
+        }
+      })
+      .catch((err) => {
         console.error(err);
-        return res.status(500).json({ error: 'Internal Server Error' });
-      }
-      res.json({ message: 'Note deleted successfully' });
-    });
+        res.status(500).json({ error: 'Internal Server Error' });
+      });
   });
-});
   
 // Route to serve the 'notes.html' page
 app.get("/notes", (req, res) => {
